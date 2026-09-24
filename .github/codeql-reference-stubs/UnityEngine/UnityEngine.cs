45ad2ab2e693324a859ac8dca14394da5aa81451
PiesP <15217557+PiesP@users.noreply.github.com>
2026-09-24 14:52:01 +0900

    ci(codeql): compile C# with API reference stubs

---
+++ .github/codeql-reference-stubs/UnityEngine/UnityEngine.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+
+[assembly: TypeForwardedTo(typeof(UnityEngine.Object))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.Component))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.Behaviour))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.MonoBehaviour))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.Coroutine))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.Events.UnityAction<,>))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.SceneManagement.Scene))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.SceneManagement.LoadSceneMode))]
+[assembly: TypeForwardedTo(typeof(UnityEngine.SceneManagement.SceneManager))]
